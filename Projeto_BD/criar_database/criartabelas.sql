6ff546188a04718483f1811a5c30fef23ac5cdef
Rafael Barboza <rafael.almeida1096@gmail.com>
2023-09-25 23:56:47 -0300

    Corrigindo erros na criação das tabelas

--- Projeto_BD/criar_database/criartabelas.sql
+++ Projeto_BD/criar_database/criartabelas.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS exercicios(
     qtd_reps INTEGER NOT NULL,
     tempo_descanso INTEGER NOT NULL,
     tecnica_avancada TEXT NOT NULL,
-    tipo_treino CHAR(1) NOT NULL CONSTRAINT tipo_check CHECK(tipo_treino i ('A', 'B', 'C'))
+    tipo_treino CHAR(1) NOT NULL CONSTRAINT tipo_check CHECK(tipo_treino in ('A', 'B', 'C'))
 ); 
 
 CREATE TABLE IF NOT EXISTS medidas(
@@ -34,7 +34,17 @@ CREATE TABLE IF NOT EXISTS treina(
     id_treino SERIAL PRIMARY KEY,
     aluno_id INT NOT NULL,
     exercicio_id INT NOT NULL,
-    CONSTRAINT fk_treino_aluno FOREIGN KEY (aluno_id) REFERENCES (id_aluno) 
+    CONSTRAINT fk_treino_aluno FOREIGN KEY (aluno_id) REFERENCES aluno(id_aluno) 
         ON DELETE CASCADE
         ON UPDATE CASCADE
 );
+
+CREATE OR REPLACE VIEW treino_aluno AS 
+    SELECT aluno.nome_aluno AS Nome, exercicios.nome_exercicio AS Exercicio, exercicios.qtd_series
+    AS Series, exercicios.qtd_reps AS Reps, exercicios.tempo_descanso AS Descanso, 
+    exercicios.tecnica_avancada AS Tecnica, exercicios.tipo_treino AS tipo_treino
+    FROM treina
+    INNER JOIN aluno
+        ON treina.aluno_id = aluno.id_aluno
+    INNER JOIN exercicios
+        ON treina.exercicio_id = exercicios.id_exercicio
