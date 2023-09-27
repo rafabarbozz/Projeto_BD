530091a40b62bd1f94cd49502b836126725264dc
Rafael Barboza <rafael.almeida1096@gmail.com>
2023-09-27 10:49:34 -0300

    Corrigindo caminho do arquivo

---
+++ Projeto_BD/DataBase/DB_config/config_db.py
@@ -0,0 +1,25 @@
+from configparser import ConfigParser
+
+# Function that gets what is inside de file database.ini and associate to each
+# variable
+def config(filename="DataBase/DB_config/database.ini", section="postgresql"):
+    #creat parser
+    parser = ConfigParser()
+    
+    #read config_file
+    parser.read(filename)
+    
+    #get sextion, default to psql
+    db = {}
+    
+    #check to see if the section (psql) exists
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            db[param[0]]= param[1]
+            
+    #return error if a param, is called and not listed in .ini file
+    else:
+        raise Exception(f"Sectio n {section} not found in {filename} file")
+    
+    return db # return a dictionary
