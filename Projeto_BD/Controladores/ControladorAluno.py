f548048443c0d2edb7a7329d1dee7876d6600766
Rafael Barboza <rafael.almeida1096@gmail.com>
2023-09-27 12:25:05 -0300

    Controlador Aluno Funcionando

--- Projeto_BD/Controladores/ControladorAluno.py
+++ Projeto_BD/Controladores/ControladorAluno.py
@@ -1,107 +1,205 @@
-from psycopg import connection
-from psycopg.rows import class_row
-from Classes.Entidades import Aluno
-from fastapi import HTTPException
+from psycopg2 import connect, DatabaseError
+from DataBase.DB_config.config_db import config
 
 # Função para criar novo aluno
-def criar_aluno(nome: str, cpf: str, sexo: str, medida_id: int, conn: connection):
-    with conn.cursor(row_factory=class_row(Aluno)) as cur:
-        cur.execute("INSERT INTO aluno (nome_aluno, cpf_aluno, sexo_aluno, medida_id) "
-                    "VALUES (%s, %s, %s, %s) RETURNING * ", 
-                    nome, cpf, sexo, medida_id)
-        
-        
-        
-# Função para alterar dados
-def alterar_nome(id: int, novo_nome: str, conn: connection):
-    with conn.cursor(row_factory=class_row(Aluno)) as cur:
-        cur.execute("UPDATE aluno SET nome_aluno = %s WHERE id_aluno = %s", (novo_nome, id))
-        temp = cur.fetchone()
-        
-        if temp is None:
-            raise HTTPException(status_code=404)
-        
-        return temp
-        
-def alterar_cpf(id: int, novo_cpf: str, conn: connection):
-    with conn.cursor(row_factory=class_row(Aluno)) as cur:
-        cur.execute("UPDATE aluno SET cpf_aluno = %s WHERE id_aluno = %s", (novo_cpf, id))
-        temp = cur.fetchone()
-        
-        if temp is None:
-            raise HTTPException(status_code=404)
-        
-        return temp
+def criar_aluno(nome: str, cpf: str, sexo: str, medida_id: int): # Funcionando
+    connection = None
     
-def alterar_sexo(id: int, novo_sexo: str, conn: connection):
-    with conn.cursor(row_factory=class_row(Aluno)) as cur:
-            cur.execute("UPDATE aluno SET sexo_aluno = %s WHERE id_aluno = %s", (novo_sexo, id))
-            temp = cur.fetchone()
-            
-            if temp is None:
-                raise HTTPException(status_code=404)
-            
-            return temp
-            
-def alterar_medida_id(id: int, novo_medida_id: int, conn: connection):
-    with conn.cursor(row_factory=class_row(Aluno)) as cur:
-        cur.execute("UPDATE aluno SET medida_id = %s WHERE id_aluno = %s", (novo_medida_id, id))
-        temp = cur.fetchone()
+    try:
+        params = config()
+        with connect(**params) as conn:
+            with conn.cursor() as cur:
+                insert_script = 'INSERT INTO aluno (nome_aluno, cpf_aluno, sexo_aluno, medida_id) VALUES (%s, %s, %s, %s) RETURNING *'
+                insert_value = (nome, cpf, sexo, medida_id)
+                cur.execute(insert_script, insert_value)
+                
+                return cur.fetchone()
         
-        if temp is None:
-            raise HTTPException(status_code=404)
+    except (Exception, DatabaseError) as error:
+        print(error)
         
-        return temp
+    finally:
+        if connection is not None:
+            connection.close()
+        
+        
+        
+# Função para alterar dados 
+def alterar_nome(id: int, novo_nome: str): # Funcionando
+    connection = None
+    
+    try:
+        params = config()
+        with connect(**params) as conn:
+            with conn.cursor() as cur:
+                insert_script = 'UPDATE aluno SET nome_aluno = %s WHERE id_aluno = %s RETURNING *'
+                insert_value = (novo_nome, id)
+                cur.execute(insert_script, insert_value)
+                
+                return cur.fetchone()
+                
+    except (Exception, DatabaseError) as error:
+        print(error)
+        
+    finally:
+        if connection is not None:
+            connection.close()
+        
+def alterar_cpf(id: int, novo_cpf: str): # Funcionando
+    connection = None
+    
+    try:
+        params = config()
+        with connect(**params) as conn:
+            with conn.cursor() as cur:
+                insert_script = 'UPDATE aluno SET cpf_aluno = %s WHERE id_aluno = %s RETURNING *'
+                insert_value = (novo_cpf, id)
+                cur.execute(insert_script, insert_value)
+                
+                return cur.fetchone()
+                
+    except (Exception, DatabaseError) as error:
+        print(error)
+        
+    finally:
+        if connection is not None:
+            connection.close()
+    
+def alterar_sexo(id: int, novo_sexo: str): # Funcionando
+    connection = None
+    
+    try:
+        params = config()
+        with connect(**params) as conn:
+            with conn.cursor() as cur:
+                insert_script = 'UPDATE aluno SET sexo_aluno = %s WHERE id_aluno = %s RETURNING *'
+                insert_value = (novo_sexo, id)
+                cur.execute(insert_script, insert_value)
+                
+                return cur.fetchone()
+                
+    except (Exception, DatabaseError) as error:
+        print(error)
+        
+    finally:
+        if connection is not None:
+            connection.close()
+            
+def alterar_medida_id(id: int, novo_medida_id: int): # Funcionando
+    connection = None
+    
+    try:
+        params = config()
+        with connect(**params) as conn:
+            with conn.cursor() as cur:
+                insert_script = 'UPDATE aluno SET medida_id = %s WHERE id_aluno = %s RETURNING *'
+                insert_value = (novo_medida_id, id)
+                cur.execute(insert_script, insert_value)
+                
+                return cur.fetchone()
+                
+    except (Exception, DatabaseError) as error:
+        print(error)
+        
+    finally:
+        if connection is not None:
+            connection.close()
     
     
 
 # Funções para pesquisar dados
-def pesquisar_por_nome(nome: str, conn: connection):
-    with conn.cursor(row_factory=class_row(Aluno)) as cur:
-        cur.execute("SELECT * FROM aluno WHERE nome_aluno = %", (nome,))
-        temp = cur.fetchone()
-        
-        if temp is None:
-            raise HTTPException(status_code=404)
-        
-        return temp
+def pesquisar_por_nome(nome: str): # Funcionando
+    connection = None
     
-def pesquisar_por_cpf(cpf: str, conn: connection):
-    with conn.cursor(row_factory=class_row(Aluno)) as cur:
-        cur.execute("SELECT * FROM aluno WHERE cpf_aluno = %", (cpf,))
-        temp = cur.fetchone()
+    try:
+        params = config()
+        with connect(**params) as conn:
+            with conn.cursor() as cur:
+                cur.execute('SELECT * FROM aluno WHERE nome_aluno = %s', (nome,))
+                
+                return cur.fetchone()
+                
+    except (Exception, DatabaseError) as error:
+        print(error)
         
-        if temp is None:
-            raise HTTPException(status_code=404)
+    finally:
+        if connection is not None:
+            connection.close() 
+    
+def pesquisar_por_cpf(cpf: str): # Funcionando
+    connection = None
+    
+    try:
+        params = config()
+        with connect(**params) as conn:
+            with conn.cursor() as cur:
+                cur.execute('SELECT * FROM aluno WHERE cpf_aluno = %s', (cpf,))
+                
+                return cur.fetchone()
+                
+    except (Exception, DatabaseError) as error:
+        print(error)
         
-        return temp
+    finally:
+        if connection is not None:
+            connection.close() 
+
     
     
 # Funções para remover dados
-def remover_por_nome(nome: str, conn: connection):
-    with conn.cursor(row_factory=class_row(Aluno)) as cur:
-        cur.execute("DELETE FROM aluno WHERE nome_aluno = %s RETURNING *", (nome,))
-        temp = cur.fetchone()
-        
-        if temp is None:
-            raise HTTPException(status_code=404)
-        
-        return temp
+def remover_por_nome(nome: str): # Funcionando
+    connection = None
     
-def remover_por_id(id: int, conn: connection):
-    with conn.cursor(row_factory=class_row(Aluno)) as cur:
-        cur.execute("DELETE FROM aluno WHERE id_aluno = %s RETURNING *", (id,))
-        temp = cur.fetchone()
+    try:
+        params = config()
+        with connect(**params) as conn:
+            with conn.cursor() as cur:
+                cur.execute('DELETE FROM aluno WHERE nome_aluno = %s RETURNING *', (nome,))
+                
+                return cur.fetchone()
+                
+    except (Exception, DatabaseError) as error:
+        print(error)
         
-        if temp is None:
-            raise HTTPException(status_code=404)
+    finally:
+        if connection is not None:
+            connection.close()
+    
+def remover_por_id(id: int): # Funcionando
+    connection = None
+    
+    try:
+        params = config()
+        with connect(**params) as conn:
+            with conn.cursor() as cur:
+                cur.execute('DELETE FROM aluno WHERE id_aluno = %s RETURNING *', (id,))
+                
+                return cur.fetchone()
+                
+    except (Exception, DatabaseError) as error:
+        print(error)
         
-        return temp
+    finally:
+        if connection is not None:
+            connection.close()
 
 
 
 # Função para listar tudo
-def listar_exercicios(conn: connection):
-    with conn.cursor(row_factory=class_row(Aluno)) as cur:
-        cur.execute("SELECT * FROM aluno ORDER BY id_aluno")
-        return cur.fetchall()  
+def listar_exercicios(): # Funcionando
+    connection = None
+    
+    try:
+        params = config()
+        with connect(**params) as conn:
+            with conn.cursor() as cur:
+                cur.execute('SELECT * FROM aluno ORDER BY id_aluno')
+                
+                return cur.fetchall()
+                
+    except (Exception, DatabaseError) as error:
+        print(error)
+        
+    finally:
+        if connection is not None:
+            connection.close()
